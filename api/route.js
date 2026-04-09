240b82217cc9912bdb5d7872a2a666bd1ed84a15
Emre ÖZTEMİZ <emreoztemiz.ai.ml@gmail.com>
2026-04-09 11:36:25 +0300

    Update route.js

--- api/route.js
+++ api/route.js
@@ -1,29 +1,20 @@
-// api/route.js
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
-export default async function handler(req, res) {
-  try {
-    const response = await fetch(
-      "https://api.openrouteservice.org/v2/directions/cycling-regular",
-      {
-        method: "POST",
-        headers: {
-          "Authorization": process.env.ORS_API_KEY, // API key buradan okunacak
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          coordinates: [
-            [30.5592, 36.5983], // Kemer
-            [30.7262, 36.9634]  // Varsak
-          ]
-        })
-      }
-    );
-
-    const data = await response.json();
-    res.status(200).json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Server fetch failed" });
-  }
+export default async function handler(req,res){
+  const ORS_KEY = process.env.ORS_API_KEY;
+  const body = {
+    coordinates:[[30.5592,36.5983],[30.7262,36.9634]],
+    profile:"cycling-regular",
+    format:"geojson"
+  };
+  const response = await fetch('https://api.openrouteservice.org/v2/directions/cycling-regular',{
+    method:'POST',
+    headers:{
+      'Authorization':ORS_KEY,
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify(body)
+  });
+  const data = await response.json();
+  res.status(200).json(data.features[0].geometry);
 }
